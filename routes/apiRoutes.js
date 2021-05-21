8fdf1be7d1bbaf530fe90fc3869a05b1da1e3143
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 15:54:38 -0400

    add notearray

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 var noteData = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+var noteArray = noteData.results;
 
 module.exports = (app) => {
-    console.log(noteData);
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => res.json(noteArray));
 
     app.post('/api/notes', (req, res) => {
-        console.log(noteData);
+        console.log(noteArray);
         noteData.push(req.body);
         res.json(true);
-        console.lot(noteData);
+        console.lot(noteArray);
     });
 };
