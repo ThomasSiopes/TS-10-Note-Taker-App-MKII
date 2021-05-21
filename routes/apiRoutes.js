89710a650f6a625c33718f6bccb1c2c41e7c3c97
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 15:27:59 -0400

    fixed json reading

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
-const noteData = require('../db/db.json');
+let noteData = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData));
 
     app.post('/api/notes', (req, res) => {
+        console.log(noteData);
         noteData.push(req.body);
         res.json(true);
+        console.lot(noteData);
     });
 };
