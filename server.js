d98c640ee326869b1b95f47e23e9279e2d0a7356
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 15:02:54 -0400

    fixed static includes

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 require('./routes/htmlRoutes')(app);
 
