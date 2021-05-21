c5ffeff8f6158b0cfe81ae94aeec79b2aa56315b
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 16:14:29 -0400

    removed test data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,12 @@
-// const fs = require('fs');
-// var noteData = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
-// var noteArray = noteData.results;
-
-const noteData = require('../public/noteData');
+const fs = require('fs');
+var noteData = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+var noteArray = noteData.results;
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => res.json(noteArray));
 
     app.post('/api/notes', (req, res) => {
-        console.log(noteArray);
         noteData.push(req.body);
         res.json(true);
-        console.lot(noteArray);
     });
 };
