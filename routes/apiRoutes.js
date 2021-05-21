896a26aa97034941e1d0bebad477933a535279be
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 16:50:08 -0400

    guesswork.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 const noteData = fs.readFileSync(path.join(__dirname, '..', 'db', 'db.json'));
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => res.json(noteData.results));
 
     app.post('/api/notes', (req, res) => {
         noteData.push(req.body);
