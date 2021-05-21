fddc6ff51c08303e2b4a088143c38efc82e88742
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 16:39:14 -0400

    add dirname please work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const noteData = fs.readFileSync('../db/db');
+const noteData = fs.readFileSync(__dirname + '/db/db');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData));
