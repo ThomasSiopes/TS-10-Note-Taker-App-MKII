28cdefa4e68524ede21df0fe7e7a86fb5b122259
theeshyguy <t.siopes00@yahoo.com>
2021-05-21 17:11:57 -0400

    added test line

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const noteData = fs.readFileSync(path.join(__dirname, '..', 'db', 'db.json'));
+const noteData = fs.readFileSync(path.join(__dirname, '..', 'db', 'db.json'), 'utf8');
+console.log(noteData);
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData.results));
@@ -9,4 +10,6 @@ module.exports = (app) => {
         noteData.push(req.body);
         res.json(true);
     });
+    
+    // app.delete('/api/notes', (req, res) => {});
 };
